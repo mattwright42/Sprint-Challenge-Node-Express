9f40a971759c932ede78ee02913334b0a65c73f5
Matthew Wright <matthewbwright@gmail.com>
2018-11-09 09:59:25 -0700

    creates GET for action id

--- api/server.js
+++ api/server.js
@@ -19,13 +19,32 @@ server.get('/api/actions', (req, res) => {
     .get()
     .then(actions => res.status(200).json(actions))
     .catch(err =>
-      res
-        .status(500)
-        .json({
-          message: 'The action information could not be retrieved.',
-          error: err
-        })
+      res.status(500).json({
+        message: 'The action information could not be retrieved.',
+        error: err
+      })
     );
+});
+
+server.get('/api/actions/:id', (req, res) => {
+  const { id } = req.params;
+  action
+    .get(id)
+    .then(action => {
+      if (action) {
+        res.status(200).json(action);
+      } else {
+        res.status(404).json({
+          message: 'The action with the specified ID does not exist.'
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'The action information could not be retrieved.',
+        error: err
+      });
+    });
 });
 
 // routes for projects
