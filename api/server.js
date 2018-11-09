b28852481fe107095019f935de31d754454997f9
Matthew Wright <matthewbwright@gmail.com>
2018-11-09 09:54:57 -0700

    creates GET for actions

--- api/server.js
+++ api/server.js
@@ -13,4 +13,21 @@ server.get('/', (req, res) => {
   res.status(200).json({ api: 'Here we go!' });
 });
 
+// routes for actions
+server.get('/api/actions', (req, res) => {
+  action
+    .get()
+    .then(actions => res.status(200).json(actions))
+    .catch(err =>
+      res
+        .status(500)
+        .json({
+          message: 'The action information could not be retrieved.',
+          error: err
+        })
+    );
+});
+
+// routes for projects
+
 module.exports = server;
